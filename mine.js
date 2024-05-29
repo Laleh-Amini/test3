437d5c326b37e890708ad8627e06036bea5a4895
Laleh-Amini <130856524+Laleh-Amini@users.noreply.github.com>
2024-05-29 15:51:20 +0330

    Add files via upload

---
+++ mine.js
@@ -0,0 +1,8 @@
+ /* instagram : @jamshidghafari.js */
+    /* جمشید غفاری */
+    $('.share').on('click', function(){
+  $(this).toggleClass('scale');
+  $('div#hide').removeAttr('id');
+  $('.active').toggleClass('open');
+});
+
